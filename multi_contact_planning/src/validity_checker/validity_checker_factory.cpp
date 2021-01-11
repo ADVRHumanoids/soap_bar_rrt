35d416626ab6e27e673882c77d8a4b8765170190
Luca Rossini <luca.rossini@iit.it>
2021-01-11 10:05:52 +0100

    uncommented publish() function

--- multi_contact_planning/src/validity_checker/validity_checker_factory.cpp
+++ multi_contact_planning/src/validity_checker/validity_checker_factory.cpp
@@ -7,6 +7,9 @@
 
 #include <boost/math/constants/constants.hpp>
 
+#include <thread>
+#include <chrono>
+
 namespace
 {
 
@@ -100,7 +103,7 @@ std::function<bool ()> MakeCentroidalStaticsChecker(YAML::Node vc_node,
 
     auto validity_checker = [=]()
     {
-//        cs_ros->publish();
+        cs_ros->publish();
         return cs->checkStability(eps);
     };
 
@@ -142,12 +145,12 @@ std::function<bool ()> MakeDistanceCheck_tripod(YAML::Node planner_config,
                                                 XBot::ModelInterface::Ptr model)
 {
     using namespace XBot::Cartesian::Planning;
-
+    
     YAML_PARSE_OPTION(vc_node, min_x_distance, double, 0.0);
     YAML_PARSE_OPTION(vc_node, min_y_distance, double, 0.0);
     YAML_PARSE_OPTION(planner_config["state_space"], ee_number, int, 2);
     YAML_PARSE_OPTION(planner_config["state_space"], end_effector, std::vector<std::string>, {});
-
+    
     auto validity_checker = [=]()
     {
         std::vector<Eigen::Vector2d> ee(ee_number);
@@ -158,26 +161,26 @@ std::function<bool ()> MakeDistanceCheck_tripod(YAML::Node planner_config,
             model->getPose(end_effector[i], T);
             ee[i] << T.translation().x(), T.translation().y();
         }
-
+        
         for (int i = 0; i < ee_number-1; i++)
         {
             for (int j = i+1; j < ee_number; j++)
             {
                 double x_distance = sqrt((ee[i](0) - ee[j](0)) * (ee[i](0) - ee[j](0)));
                 double y_distance = sqrt((ee[i](1) - ee[j](1)) * (ee[i](1) - ee[j](1)));
-
+                                
                 if (x_distance < min_x_distance && y_distance < min_y_distance)
                 {
                     return false;
                 }
             }
         }
-
+        
         return true;
     };
-
+    
     return validity_checker;
-
+    
 }
 
 }
@@ -185,30 +188,30 @@ std::function<bool ()> MakeDistanceCheck_tripod(YAML::Node planner_config,
 std::function<bool ()> MakeDistanceCheck_centauro(YAML::Node planner_config,
                                                   YAML::Node vc_node,
                                                   XBot::ModelInterface::Ptr model)
-{
+{ 
     using namespace XBot::Cartesian::Planning;
-
+    
     YAML_PARSE_OPTION(vc_node, max_x_distance, double, 0.0);
     YAML_PARSE_OPTION(vc_node, max_y_distance, double, 0.0);
     YAML_PARSE_OPTION(vc_node, front_rear_x_distance, double, 0.0);
     YAML_PARSE_OPTION(vc_node, left_right_y_distance, double, 0.0);
     YAML_PARSE_OPTION(planner_config["state_space"], ee_number, int, 2);
     YAML_PARSE_OPTION(planner_config["state_space"], end_effector, std::vector<std::string>, {});
-
+       
     auto validity_checker = [=]()
     {
         std::vector<Eigen::Vector2d> ee(ee_number);
         Eigen::Affine3d T;
-
+        
         for (int i = 0; i < ee_number; i++)
         {
             model->getPose(end_effector[i], T);
             ee[i] << T.translation().x(), T.translation().y();
         }
-
-        double x_diff;
-        double y_diff;
-
+        
+        double x_diff; 
+        double y_diff;                  
+            
         for (int i = 0; i < ee_number; i += 2)
         {
             x_diff = sqrt((ee[i](0) - ee[i+1](0)) * (ee[i](0) - ee[i+1](0)));
@@ -218,107 +221,107 @@ std::function<bool ()> MakeDistanceCheck_centauro(YAML::Node planner_config,
                 return false;
             }
         }
-
+            
         // Check feet crossing
         double xRel_w;
         double yRel_w;
-
+            
         for (int i = 0; i < ee_number; i += 2)
         {
             xRel_w = ee[i](0) - ee[i+1](0);
             yRel_w = ee[i](1) - ee[i+1](1);
-            if (yRel_w < 0.1)
+            if (yRel_w < 0.1)          
             {
-                return false;
+                return false;        
             }
         }
-
+         
         // Check distance between front and rear feet on x-axis
         const auto x_left = sqrt((ee[0](0) - ee[2](0)) * (ee[0](0) - ee[2](0)));
         const auto x_right = sqrt((ee[1](0) - ee[3](0)) * (ee[1](0) - ee[3](0)));
         const auto x_left_right = sqrt((ee[0](0) - ee[3](0)) * (ee[0](0) - ee[3](0)));
         const auto x_right_left = sqrt((ee[1](0) - ee[2](0)) * (ee[1](0) - ee[2](0)));
-
+          
         if (x_right < front_rear_x_distance || x_left < front_rear_x_distance || x_right > 0.9 || x_left > 0.9 || x_left_right < front_rear_x_distance || x_right_left < front_rear_x_distance || x_right_left > 0.8 || x_left_right > 0.8)
         {
             return false;
         }
-
+            
         // Check distance between front and rear feet on y-axis
         const auto y_front = sqrt((ee[0](1) - ee[1](1)) * (ee[0](1) - ee[1](1)));
         const auto y_rear = sqrt((ee[2](1) - ee[3](1)) * (ee[2](1) - ee[3](1)));
         const auto y_front_rear = sqrt((ee[0](1) - ee[3](1)) * (ee[0](1) - ee[3](1)));
-        const auto y_rear_front = sqrt((ee[2](1) - ee[1](1)) * (ee[2](1) - ee[1](1)));
-
+        const auto y_rear_front = sqrt((ee[2](1) - ee[1](1)) * (ee[2](1) - ee[1](1)));        
+            
         if (y_front < left_right_y_distance || y_rear < left_right_y_distance || y_front > 0.8 || y_rear > 0.8 || y_front_rear < left_right_y_distance || y_front_rear > 0.8 || y_rear_front < left_right_y_distance || y_rear_front > 0.8)
         {
             return false;
         }
-
+        
         return true;
     };
-
+    
     return validity_checker;
-
+    
 }
 
 std::function<bool ()> MakeDistanceCheck_comanplus(YAML::Node planner_config,
                                                YAML::Node vc_node,
                                                XBot::ModelInterface::Ptr model)
-{
+{ 
     using namespace XBot::Cartesian::Planning;
-
+    
     YAML_PARSE_OPTION(vc_node, max_x_distance, double, 0.0);
     YAML_PARSE_OPTION(vc_node, max_y_distance, double, 0.0);
     YAML_PARSE_OPTION(planner_config["state_space"], ee_number, int, 2);
     YAML_PARSE_OPTION(planner_config["state_space"], end_effector, std::vector<std::string>, {});
-
+       
     auto validity_checker = [=]()
     {
         std::vector<Eigen::Vector3d> ee(ee_number);
         Eigen::Affine3d T;
-
+        
         for (int i = 0; i < ee_number; i++)
         {
             model->getPose(end_effector[i], T);
             auto rot = T.linear().eulerAngles(0, 1, 2);
             ee[i] << T.translation().x(), T.translation().y(), rot(2);
-
+            
         }
-
-        double x_diff = sqrt((ee[0](0) - ee[1](0)) * (ee[0](0) - ee[1](0)));
+        
+        double x_diff = sqrt((ee[0](0) - ee[1](0)) * (ee[0](0) - ee[1](0))); 
         double y_diff = sqrt((ee[0](1) - ee[1](1)) * (ee[0](1) - ee[1](1)));
 
         if (x_diff > max_x_distance || y_diff > max_y_distance)
         {
             std::cout << "x_diff = " << x_diff << "    y_diff = " << y_diff << std::endl;
             return false;
-        }
-
+        }           
+            
         // Check for relative orientation
         double res1 = (ee[0](2) - ee[1](2));
-        double res2 = (boost::math::constants::pi<double>()*2 - (ee[0](2) - ee[1](2)));
-
+        double res2 = (boost::math::constants::pi<double>()*2 - (ee[0](2) - ee[1](2))); 
+        
         if (std::min<double>(sqrt(res1*res1), sqrt(res2*res2)) > boost::math::constants::pi<double>()/6)
         {
             std::cout << "orientation foot 1: " << ee[0](2) << "    orientation foot 2:" << ee[1](2) << std::endl;
             std::cout << "res = " << std::min<double>(sqrt(res1*res1), sqrt(res2*res2)) << std::endl;
             return false;
-        }
-
+        }           
+             
         // Check for feet crossing
         double xRel_w = ee[0](0) - ee[1](0);
         double yRel_w = ee[0](1) - ee[1](1);
-
-        if (-xRel_w * sin(ee[1](2)) + yRel_w * cos(ee[1](2)) > 0.20)
+    
+        if (-xRel_w * sin(ee[1](2)) + yRel_w * cos(ee[1](2)) > 0.20)                
         {
             std::cout << "relative orientation = " << -xRel_w * sin(ee[1](2)) + yRel_w * cos(ee[1](2)) << std::endl;
-            return false;
-        }
-
+            return false;        
+        }           
+        
         return true;
     };
-
+    
     return validity_checker;
 }
 
