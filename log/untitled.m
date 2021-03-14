fe4bab53cbd44bf2f8ea2b36d82bffde8d8741f5
Luca Rossini <luca.rossini@iit.it>
2021-03-14 20:22:24 +0100

    added log files

--- log/untitled.m
+++ log/untitled.m
@@ -7,11 +7,11 @@ average = sum(time) / length(time);
 
 plot([1:length(time)], time)
 
-% plot([1:length(time_collision_check)], time_collision_check, 'b')
+plot([1:length(time_collision_check)], time_collision_check, 'b')
 hold on
-% plot([1:length(time_centroidal_statics_check)], time_centroidal_statics_check, 'r')
-% plot([1:length(time_solve)], time_solve, 'g')
-% legend('collision check', 'centroidal statics check', 'IK solve')
+plot([1:length(time_centroidal_statics_check)], time_centroidal_statics_check, 'r')
+plot([1:length(time_solve)], time_solve, 'g')
+legend('collision check', 'centroidal statics check', 'IK solve')
 
 disp(["success rate is: ", good/length(success)*100])
 disp(["average: ", average])
