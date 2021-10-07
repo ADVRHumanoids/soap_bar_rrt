51890ebf8ec0e604741a9f948b64d93a65314e5c
Luca Rossini <luca.rossini@iit.it>
2021-10-07 11:25:15 +0200

    DIST_THRES_MIN/MAX moved in each scenario for different initialization

--- multi_contact_planning/src/planner/multi_contact/constant_values.hpp
+++ multi_contact_planning/src/planner/multi_contact/constant_values.hpp
@@ -8,8 +8,9 @@ const int MAX_ITERATIONS = 5000; //10000;
 const double EXPLOITATION_RATE = 1.0; //0.9; // between 0 and 1 (it is a threshold for the probability)
 const double GOAL_TOLERANCE = 0.05; // it means 5 cm
 const int MAX_NUM_EXP_PER_VERTEX = 5; // maximum number of times that a certain vertex can be used for an expansion attempt (both succ or not)
-const double DIST_THRES_MIN = 0.60;
-const double DIST_THRES_MAX = 1.80;
+const double DIST_THRES_MAX_HANDS = 0.3;
+const double DIST_THRES_MAX_FEET = 0.3;
+
 
 // cpl
 const double ROBOT_MASS = 70.0;
@@ -28,14 +29,14 @@ const double CS_THRES = 0.5*1e-3;
 
 // nspg
 const bool RAND_VEL_CHAINS = false;
-const double DT = 0.005; //0.001
+const double DT = 0.001; //0.001
 const int ITER_MAX = 100;
-const double GAIN_VEL_FB_X = 150; //500;
-const double GAIN_VEL_FB_Y = 250; //250;
-const double GAIN_VEL_FB_Z = 150; //100;
-const double FORCE_THRES = 70.0;
-const double CoP_LIM_X = 0.09;
-const double CoP_LIM_Y = 0.09;
+const double GAIN_VEL_FB_X = 100; //500;
+const double GAIN_VEL_FB_Y = 150; //250;
+const double GAIN_VEL_FB_Z = 100; //100;
+const double FORCE_THRES = 40.0;
+const double CoP_LIM_X = 0.08;
+const double CoP_LIM_Y = 0.035;
 
 /*
 // SCENARIO 1, PHASE 0
@@ -49,35 +50,41 @@ const double WORKSPACE_RADIUS_HAND = 0.63;
 const double GOAL_SAMPLER_TIME_BUDGET = 3.0;
 const bool FREE_YAW_ROTATION = false;
 const double DIST_HANDS_THRES_MAX = 1.0;
+const double DIST_THRES_MIN = 0.60;
+const double DIST_THRES_MAX = 1.80;
 */
 
-/*
+
 // SCENARIO 1, PHASE 1
 const int SCENARIO = 1;
 const int INIT_INDEX = 1;
 const int GOAL_INDEX = 2;
-const double MU_FRICTION = 0.5;  
-const double MU_FRICTION_HANDS = 0.5;  
-const double WORKSPACE_RADIUS_FOOT = 0.23;  
+const double MU_FRICTION = 0.5;
+const double MU_FRICTION_HANDS = 0.5;
+const double WORKSPACE_RADIUS_FOOT = 0.23;
 const double WORKSPACE_RADIUS_HAND = 0.23;
 const double GOAL_SAMPLER_TIME_BUDGET = 3.0;
 const bool FREE_YAW_ROTATION = false;
 const double DIST_HANDS_THRES_MAX = 1.0;
-*/
+const double DIST_THRES_MIN = 0.50;
+const double DIST_THRES_MAX = 1.0;
 
-/*
+
+
 // SCENARIO 1, PHASE 2
-const int SCENARIO = 1;
-const int INIT_INDEX = 2;
-const int GOAL_INDEX = 3;
-const double MU_FRICTION = 0.5;  
-const double MU_FRICTION_HANDS = 0.5;  
-const double WORKSPACE_RADIUS_FOOT = 0.43;  
-const double WORKSPACE_RADIUS_HAND = 0.43;
-const double GOAL_SAMPLER_TIME_BUDGET = 3.0;
-const bool FREE_YAW_ROTATION = false;
-const double DIST_HANDS_THRES_MAX = 1.0;
-*/
+//const int SCENARIO = 1;
+//const int INIT_INDEX = 2;
+//const int GOAL_INDEX = 3;
+//const double MU_FRICTION = 0.5;
+//const double MU_FRICTION_HANDS = 0.5;
+//const double WORKSPACE_RADIUS_FOOT = 0.43;
+//const double WORKSPACE_RADIUS_HAND = 0.43;
+//const double GOAL_SAMPLER_TIME_BUDGET = 3.0;
+//const bool FREE_YAW_ROTATION = false;
+//const double DIST_HANDS_THRES_MAX = 1.0;
+//const double DIST_THRES_MIN = 0.60;
+//const double DIST_THRES_MAX = 1.80;
+
 
 /* //LUCA
 const int SCENARIO = 1;
@@ -92,17 +99,18 @@ const bool FREE_YAW_ROTATION = false;
 const double DIST_HANDS_THRES_MAX = 1.0;
 */
 
-
+/*
 const int SCENARIO = 1;
 const int INIT_INDEX = 2;
 const int GOAL_INDEX = 3;
-const double MU_FRICTION = 0.5;  
-const double MU_FRICTION_HANDS = 0.5;  
+const double MU_FRICTION = 0.5;
+const double MU_FRICTION_HANDS = 0.5;
 const double WORKSPACE_RADIUS_FOOT = 0.3;
 const double WORKSPACE_RADIUS_HAND = 0.8;
 const double GOAL_SAMPLER_TIME_BUDGET = 3.0;
 const bool FREE_YAW_ROTATION = false;
 const double DIST_HANDS_THRES_MAX = 1.0;
+*/
 
 /*
 // SCENARIO 2, PHASE 0
@@ -116,6 +124,8 @@ const double WORKSPACE_RADIUS_HAND = 0.63;
 const double GOAL_SAMPLER_TIME_BUDGET = 3.0;
 const bool FREE_YAW_ROTATION = false;
 const double DIST_HANDS_THRES_MAX = 1.0;
+const double DIST_THRES_MIN = 0.60;
+const double DIST_THRES_MAX = 1.80;
 */
 
 /*
@@ -130,6 +140,8 @@ const double WORKSPACE_RADIUS_HAND = 0.23;
 const double GOAL_SAMPLER_TIME_BUDGET = 3.0;
 const bool FREE_YAW_ROTATION = false;
 const double DIST_HANDS_THRES_MAX = 1.0;
+const double DIST_THRES_MIN = 0.60;
+const double DIST_THRES_MAX = 1.80;
 */
 
 /*
@@ -144,6 +156,8 @@ const double WORKSPACE_RADIUS_HAND = 0.23;
 const double GOAL_SAMPLER_TIME_BUDGET = 3.0;
 const bool FREE_YAW_ROTATION = false;
 const double DIST_HANDS_THRES_MAX = 1.0;
+const double DIST_THRES_MIN = 0.60;
+const double DIST_THRES_MAX = 1.80;
 */
 
 #endif
