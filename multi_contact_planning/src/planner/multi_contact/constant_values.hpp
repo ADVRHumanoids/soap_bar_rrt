72d2bcc1a3db94807d7c87defd6b25d95846f706
Luca Rossini <luca.rossini@iit.it>
2021-10-29 16:33:41 +0200

    modified constant values

--- multi_contact_planning/src/planner/multi_contact/constant_values.hpp
+++ multi_contact_planning/src/planner/multi_contact/constant_values.hpp
@@ -7,7 +7,7 @@ const int NUM_SIM = 1;
 const int MAX_ITERATIONS = 5000; //10000;
 const double EXPLOITATION_RATE = 1.0; //0.9; // between 0 and 1 (it is a threshold for the probability)
 const double GOAL_TOLERANCE = 0.05; // it means 5 cm
-const int MAX_NUM_EXP_PER_VERTEX = 5; // maximum number of times that a certain vertex can be used for an expansion attempt (both succ or not)
+const int MAX_NUM_EXP_PER_VERTEX = 10; // maximum number of times that a certain vertex can be used for an expansion attempt (both succ or not)
 const double DIST_THRES_MAX_HANDS = 0.3;
 const double DIST_THRES_MAX_FEET = 0.3;
 
@@ -29,12 +29,12 @@ const double CS_THRES = 0.5*1e-2;
 
 // nspg
 const bool RAND_VEL_CHAINS = false;
-const double DT = 0.001; //0.001
+const double DT = 0.002; //0.001
 const int ITER_MAX = 100;
 const double GAIN_VEL_FB_X = 100; //500;
 const double GAIN_VEL_FB_Y = 100; //250;
 const double GAIN_VEL_FB_Z = 50; //100;
-const double FORCE_THRES = 20.0;
+const double FORCE_THRES = 10.0;
 const double CoP_LIM_X = 0.08;
 const double CoP_LIM_Y = 0.035;
 
@@ -56,34 +56,34 @@ const double CoP_LIM_Y = 0.035;
 
 
 // SCENARIO 1, PHASE 1
-//const int SCENARIO = 1;
-//const int INIT_INDEX = 1;
-//const int GOAL_INDEX = 2;
-//const double MU_FRICTION = 0.5;
-//const double MU_FRICTION_HANDS = 0.5;
-//const double WORKSPACE_RADIUS_FOOT = 0.23;
-//const double WORKSPACE_RADIUS_HAND = 0.23;
-//const double GOAL_SAMPLER_TIME_BUDGET = 3.0;
-//const bool FREE_YAW_ROTATION = false;
-//const double DIST_HANDS_THRES_MAX = 1.0;
-//const double DIST_THRES_MIN = 0.50;
-//const double DIST_THRES_MAX = 1.0;
+const int SCENARIO = 1;
+const int INIT_INDEX = 1;
+const int GOAL_INDEX = 2;
+const double MU_FRICTION = 0.5;
+const double MU_FRICTION_HANDS = 0.5;
+const double WORKSPACE_RADIUS_FOOT = 0.23;
+const double WORKSPACE_RADIUS_HAND = 0.23;
+const double GOAL_SAMPLER_TIME_BUDGET = 3.0;
+const bool FREE_YAW_ROTATION = false;
+const double DIST_HANDS_THRES_MAX = 1.0;
+const double DIST_THRES_MIN = 0.6;
+const double DIST_THRES_MAX = 0.95;
 
 
 
 // SCENARIO 1, PHASE 2
-const int SCENARIO = 1;
-const int INIT_INDEX = 2;
-const int GOAL_INDEX = 3;
-const double MU_FRICTION = 0.4;
-const double MU_FRICTION_HANDS = 0.4;
-const double WORKSPACE_RADIUS_FOOT = 0.3;
-const double WORKSPACE_RADIUS_HAND = 0.8;
-const double GOAL_SAMPLER_TIME_BUDGET = 3.0;
-const bool FREE_YAW_ROTATION = false;
-const double DIST_HANDS_THRES_MAX = 1.0;
-const double DIST_THRES_MIN = 0.60;
-const double DIST_THRES_MAX = 1.80;
+//const int SCENARIO = 1;
+//const int INIT_INDEX = 2;
+//const int GOAL_INDEX = 3;
+//const double MU_FRICTION = 0.4;
+//const double MU_FRICTION_HANDS = 0.4;
+//const double WORKSPACE_RADIUS_FOOT = 0.3;
+//const double WORKSPACE_RADIUS_HAND = 0.8;
+//const double GOAL_SAMPLER_TIME_BUDGET = 3.0;
+//const bool FREE_YAW_ROTATION = false;
+//const double DIST_HANDS_THRES_MAX = 1.0;
+//const double DIST_THRES_MIN = 0.60;
+//const double DIST_THRES_MAX = 1.80;
 
 
 /* //LUCA
